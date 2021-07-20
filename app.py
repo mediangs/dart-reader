18a017dcee13e8738e3ecc9f03fb58893a7ee61a
Lee Jongki <jongki@nate.com>
2021-07-20 10:46:07 +0900

    interface change

--- app.py
+++ app.py
@@ -17,11 +17,18 @@ def app():
     global corps_loaded
 
     quant_data = st.cache(pd.read_csv)('./data/quant-20212Q.csv')
-    st.write(quant_data)
+    category = quant_data['업종 (대)'].unique()
+
+    select_category = st.sidebar.selectbox('업종선택(2021.1Q) 적자순', category)
+    st.write(quant_data.loc[quant_data['업종 (대)'] == select_category])
+
+    #st.write(quant_data)
 
     # Open DART API KEY 설정
     #api_key = st.text_input("Enter Dart api key")
-    api_key = st.secrets["api_key"]
+    from key import api_key
+
+    #api_key = st.secrets["api_key"]
 
 
     if len(api_key) > 0:
@@ -36,23 +43,26 @@ def app():
                  if c.stock_code is not None ]
         names = sorted(names)
 
-        start = st.number_input("Start year", 2015)
-        selected_company = st.selectbox('select a company', names)
+        start = st.sidebar.number_input("Start year", 2015)
+        selected_company = st.sidebar.selectbox('select a company', names)
         company = corp_list.find_by_stock_code(selected_company[-6:])
 
         naver_url = f'<a href="https://finance.naver.com/item/main.nhn?code={company.stock_code}" target="_blank" rel="noopener noreferrer">네이버 금융링크</a>'
-        st.markdown(naver_url, unsafe_allow_html=True)
+        st.sidebar.markdown(naver_url, unsafe_allow_html=True)
 
-        if st.button('년간 사업보고서 : Get data!'):
-            st.subheader(f'{selected_company}')
+        if st.sidebar.button('년간 사업보고서'):
+            st.subheader(f'{selected_company} 년간 사업보고서')
             company_sheet = yearly_company_performance(company, start, 2021, opendart)
             st.write(company_sheet)
 
-        if st.button('분기별 보고서 : Get data!'):
-            st.subheader(f'{selected_company}')
+        if st.sidebar.button('분기별 보고서'):
+            st.subheader(f'{selected_company} 분기별 보고서')
             q_company_sheet = quarterly_company_performance(company, start, 2021, opendart)
             #components.html(q_company_sheet.to_html())
             st.write(q_company_sheet)
+
+
+
 
 
 if __name__ == '__main__':
